ad7242feb3fec77d0c282f1853d2d39118cd6eda
Sugeng Priyanto <54300975+sugengpriyanto@users.noreply.github.com>
2021-12-31 15:47:35 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b5c2a160.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
